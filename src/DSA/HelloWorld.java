18f2c164e8dbbbb7d845fa8d793885d950c89914
koyna15 <koynasoni16@gmail.com>
2025-06-28 13:03:08 +0530

    Initial Commit

--- src/DSA/HelloWorld.java
+++ src/DSA/HelloWorld.java
@@ -1,7 +1,18 @@
 package DSA;
-
+import java.util.Scanner;
 public class HelloWorld {
-    public static void main(String[] args) {
-        
-    }
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        int n = s.nextInt();
+//        char[] ch = new char[n];
+//        //int n = s.nextInt();
+//        for(int i=0;i<n;i++){
+//            ch[i]=s.next().charAt(0);
+//        }
+//        for (char c : ch) {
+//            System.out.print(c + " ");
+//        }
+//        //System.out.println(ch);
+//    }
+     
 }
