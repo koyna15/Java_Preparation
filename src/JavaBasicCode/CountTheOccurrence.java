8f246be7bbee85bd5f37b2e612c079e1cda3088c
koyna15 <koynasoni16@gmail.com>
2025-07-01 09:19:40 +0530

    Initial Commit

---
+++ src/JavaBasicCode/CountTheOccurrence.java
@@ -0,0 +1,20 @@
+package JavaBasicCode;
+import java.util.*;
+public class CountTheOccurrence {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int num = in.nextInt();
+        int num = 5657777;
+        int count = 0;
+        int rem;
+
+        while(num>0){
+            rem=num%10;
+            if(rem==7){
+                count++;
+            }
+            num/=10;
+        }
+        System.out.println("Count of number is: " +count);
+    }
+}
