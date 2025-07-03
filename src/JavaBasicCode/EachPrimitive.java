f950416aac3aa7b4c573e189d9613a167fbd996f
koyna15 <koynasoni16@gmail.com>
2025-07-03 15:02:53 +0530

    Initial Commit

---
+++ src/JavaBasicCode/EachPrimitive.java
@@ -0,0 +1,16 @@
+package JavaBasicCode;
+import java.util.*;
+public class EachPrimitive {
+    public static void main(String[] args) {
+        int age = 26;
+        float marks = 48.97f;
+        double Price= 567.998;
+        char c = 'a';
+        boolean check = true;
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(Price);
+        System.out.println(c);
+        System.out.println(check);
+    }
+}
