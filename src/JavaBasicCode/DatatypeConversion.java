2f1ed2650e2b6ef28d354976c2c158e57e73ef45
koyna15 <koynasoni16@gmail.com>
2025-07-03 15:11:24 +0530

    Initial Commit

---
+++ src/JavaBasicCode/DatatypeConversion.java
@@ -0,0 +1,12 @@
+package JavaBasicCode;
+
+public class DatatypeConversion {
+    public static void main(String[] args) {
+        int num =67;
+        System.out.println(num);
+        System.out.println((float)num);
+        int num2 = 73;
+        System.out.println(num2);
+        System.out.println((double)num2);
+    }
+}
