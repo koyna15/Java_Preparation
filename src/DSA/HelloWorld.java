5d70874493175f092940449187b172f545dfc92b
KOYNA SONI <152266789+koyna15@users.noreply.github.com>
2025-07-03 14:53:19 +0530

    Update HelloWorld.java

--- src/DSA/HelloWorld.java
+++ src/DSA/HelloWorld.java
@@ -1,18 +1,9 @@
 package DSA;
 import java.util.Scanner;
 public class HelloWorld {
-//    public static void main(String[] args) {
-//        Scanner s = new Scanner(System.in);
-//        int n = s.nextInt();
-//        char[] ch = new char[n];
-//        //int n = s.nextInt();
-//        for(int i=0;i<n;i++){
-//            ch[i]=s.next().charAt(0);
-//        }
-//        for (char c : ch) {
-//            System.out.print(c + " ");
-//        }
-//        //System.out.println(ch);
-//    }
+   public static void main(String[] args) {
+       
+       System.out.println("Hello, World!);
+   }
      
 }
