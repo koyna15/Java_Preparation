b0ff70c8248f8cc0afd95eff88e5dfbc4e636263
koyna15 <koynasoni16@gmail.com>
2025-07-05 00:47:54 +0530

    Arrays

---
+++ src/DSA/Array.java
@@ -0,0 +1,10 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        System.out.println(Arrays.toString(arr));
+    }
+}
