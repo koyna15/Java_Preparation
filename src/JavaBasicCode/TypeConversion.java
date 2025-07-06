6da1468cd80963156113f7e419ac88be47dc20d2
koyna15 <koynasoni16@gmail.com>
2025-07-06 13:18:27 +0530

    Type Conversion

---
+++ src/JavaBasicCode/TypeConversion.java
@@ -0,0 +1,12 @@
+package JavaBasicCode;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        float num =98.67f;
+        System.out.println(num);
+        System.out.println((int)num);
+        int m=83;
+        System.out.println(m);
+        System.out.println((double)m);
+    }
+}
