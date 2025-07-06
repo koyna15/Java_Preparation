76bf93705afef9ca1e6926d851c7b1df6ef5501f
koyna15 <koynasoni16@gmail.com>
2025-07-06 13:24:35 +0530

    Zero At the end of array

---
+++ src/Array/ZeroAtEnd.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ZeroAtEnd {
+    public static void main(String[] args) {
+        int[] arr = {1,0,3,0,5,0,7,0};
+        int start=0, end=arr.length - 1;
+        while(start<=end){
+            if(arr[end]==0){
+                end--;
+            }
+            if(arr[start]!=0){
+                start++;
+            }
+            if(arr[start]==0 && arr[end]!=0){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
