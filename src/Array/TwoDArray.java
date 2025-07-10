a0e8c7f6fe482011a9c60e2e488ef0bbec1d0a91
koyna15 <koynasoni16@gmail.com>
2025-07-10 22:39:11 +0530

    2D Array

--- src/Array/TwoDArray.java
+++ src/Array/TwoDArray.java
@@ -16,7 +16,7 @@ public class TwoDArray {
             for(int j=0;j<arr[i].length;j++){
                 System.out.print(arr[i][j] +" ");
             }
-            System.out.println();
+            //System.out.println();
         }
         for (int i=0;i<arr.length;i++){
             System.out.println(Arrays.toString(arr[i]));
