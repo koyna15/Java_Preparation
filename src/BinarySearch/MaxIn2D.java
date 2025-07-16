9c730ff67be23ffb34ac2bee589b709fea985de3
koyna15 <koynasoni16@gmail.com>
2025-07-17 02:45:31 +0530

    Max in matrix

---
+++ src/BinarySearch/MaxIn2D.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class MaxIn2D {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {3,65,91},
+                {75,38,56,177},
+                {33,45}
+        };
+        int ans = search(arr);
+        System.out.println(ans);
+    }
+    static int search(int[][] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                }
+
+            }
+        }
+        return max;
+    }
+}
