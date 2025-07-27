2b5cef626dc47ba9071b1f912b8d54c540262c45
koyna15 <koynasoni16@gmail.com>
2025-07-28 01:34:50 +0530

    Pattern

---
+++ src/Pattern/Pattern5.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for (int row = 1; row <= 2*n; row++) {
+            int totalColsInRow = row > n ? 2*n - row : row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
