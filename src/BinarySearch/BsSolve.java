649f6d84bdceae3ff362d91be65fdcf3ca6983a5
koyna15 <koynasoni16@gmail.com>
2025-07-29 13:16:27 +0530

    Bs

--- src/BinarySearch/BsSolve.java
+++ src/BinarySearch/BsSolve.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BsSolve {
     public static void main(String[] args) {
         int[] arr = {-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
-        int target = -4;
+        int target = -18;
         int ans = binarySearch(arr,target);
         System.out.println(ans);
     }
