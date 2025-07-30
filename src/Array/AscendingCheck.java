02237ab62f630f43d1f45431c36ed1e1852c2b7f
koyna15 <koynasoni16@gmail.com>
2025-07-30 14:23:07 +0530

    Ascending

---
+++ src/Array/AscendingCheck.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class AscendingCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] d = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                d[i] = scanner.nextInt();
+            }
+            boolean isAsc = true;
+            for(int i=0;i<n-1;i++){
+                if(d[i] > d[i+1]){
+                    isAsc = false;
+                    break;
+                }
+            }
+            if (isAsc) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
