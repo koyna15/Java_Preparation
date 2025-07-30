5d3b3be9a064831df1f91a35471f6ebf18e97835
koyna15 <koynasoni16@gmail.com>
2025-07-30 18:46:16 +0530

    Multiples

---
+++ src/JavaBasicCode/Multiples.java
@@ -0,0 +1,14 @@
+package JavaBasicCode;
+
+public class Multiples {
+    public static void main(String[] args) {
+//        int n = 6;
+//        for(int i = n;i<= n*10;i+=n){
+//            System.out.print(i +" ");
+//        }
+        int n =5;
+        for (int i = n; i <=  n*10; i+=n) {
+            System.out.println(i);
+        }
+    }
+}
