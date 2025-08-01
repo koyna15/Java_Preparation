2b55a3fc07c6328aaf3e3a5130501da7c9b7c071
koyna15 <koynasoni16@gmail.com>
2025-08-01 18:59:16 +0530

    Adding elements

---
+++ src/ArrayList/AddElements.java
@@ -0,0 +1,14 @@
+package ArrayList;
+import java.util.*;
+
+public class AddElements {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Mango");
+        list.add("Kiwi");
+        list.add("Avacado");
+        list.add("Dragon Fruit");
+        System.out.println(list);
+    }
+}
