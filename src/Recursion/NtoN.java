ab01306bb597f87f0f434b8b1e3b52cfaf17ec1f
koyna15 <koynasoni16@gmail.com>
2025-08-10 02:38:29 +0530

    Print NtoN

---
+++ src/Recursion/NtoN.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class NtoN {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+    static void funBoth(int n){
+        if(n==0) return;
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
