c125e9e62fe7fb9281ae95650b1e166e07d40be5
koyna15 <koynasoni16@gmail.com>
2025-08-10 02:38:12 +0530

    Print Nto1 reverse

---
+++ src/Recursion/Nto1Reverse.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Nto1Reverse {
+    public static void main(String[] args) {
+        funRev(5);
+    }
+    static void funRev(int n){
+        if(n==0) return;
+        funRev(n-1);
+        System.out.println(n);
+    }
+}
