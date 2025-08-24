89697ba69d91120e119292ff0781deddf46ac71a
koyna15 <koynasoni16@gmail.com>
2025-08-25 02:40:09 +0530

    Skip letters

---
+++ src/Recursion/SkipLettersFromWord.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class SkipLettersFromWord {
+    public static void main(String[] args) {
+        System.out.println(skipAppNotApple("bcaappldh"));
+    }
+    static String skipAppNotApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        if(up.startsWith("app") && !up.startsWith("apple")){
+            return skipAppNotApple(up.substring(3));
+        }
+        else{
+            return up.charAt(0) + skipAppNotApple(up.substring(1));
+        }
+    }
+}
