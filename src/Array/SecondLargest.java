46ed3eae78348318369573b22dcc02b0ecc18a9e
koyna15 <koynasoni16@gmail.com>
2025-08-29 12:39:57 +0530

    Second Largest

--- src/Array/SecondLargest.java
+++ src/Array/SecondLargest.java
@@ -4,7 +4,7 @@ import java.util.Enumeration;
 
 public class SecondLargest {
     public static void main(String[] args) {
-        int[] arr = {12,34,769,78,97};
+        int[] arr = {8, 2, 9, 4 ,9};
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
         for (int num : arr) {
