24106c63d287f3e07be24c6f8e3cb7ad573666f2
koyna15 <koynasoni16@gmail.com>
2026-01-26 22:02:03 +0530

    Two Sum

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -1,11 +1,19 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] arr = {2,7,11,15};
         int target = 9;
-        int start = 0, end = arr.length-1;
 
+        twoSum(arr,target);
+        int[] ansHash = twoSumHash(arr, target);
+        System.out.println(Arrays.toString(ansHash));
+    }
+    static void twoSum(int[] arr, int target){
+        int start = 0, end = arr.length-1;
         while(start<end){
             int sum = arr[start] + arr[end];
             if(sum == target){
@@ -18,5 +26,18 @@ public class TwoSum {
                 end--;
             }
         }
+    }
+    static int[] twoSumHash(int[] arr, int target){ // Using HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++){
+            int diff = target - arr[i];
+
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{};
     }
 }
