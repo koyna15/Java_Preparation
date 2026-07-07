8bd0345d9531cf5cae1ab10d13235bf0060f2855
koyna15 <koynasoni16@gmail.com>
2026-07-07 15:58:07 +0530

    Product Except Self

---
+++ src/GfG/ProductExceptSelf.java
@@ -0,0 +1,28 @@
+package GfG;
+
+import java.util.Arrays;
+
+class ProductExceptSelf {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int[] result = productExceptSelf(arr);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] productExceptSelf(int[] arr) {
+
+        int n = arr.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i < n; i++){
+        int product = 1;
+
+           for (int j = 0; j < n; j++) {
+               if(i!=j){
+                   product = (product * arr[j])  ;
+               }
+           }
+           ans[i] = product;
+       }
+        return ans;
+    }
+}
